ac9d9e6f074d15dfa43ca6401d6db1e48b3804b9
Miguel <miguelcroman11@gmail.com>
2023-03-22 20:07:58 +0100

    Script changes

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,32 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour
 {
     public GameObject selectedObject;
     Vector3 offset, oldPos = Vector3.zero, velocity;
+	Collider2D targetObject;
  
 
  
     void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !selectedObject)
         {
-            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+            targetObject = Physics2D.OverlapPoint(mousePosition);
+			print(targetObject == null);
             if (targetObject)
             {
                 selectedObject = targetObject.transform.gameObject;
+				selectedObject.transform.SetAsLastSibling();
+				targetObject.enabled = false;
                 oldPos = selectedObject.transform.position;
-                selectedObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                //selectedObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 offset = selectedObject.transform.position - mousePosition;
             }
         }
         if (Input.GetMouseButtonUp(0) && selectedObject)
         {
             velocity = selectedObject.transform.position - oldPos;
-            selectedObject.GetComponent<Rigidbody2D>().velocity = velocity;
+            selectedObject.GetComponent<Rigidbody2D>().velocity = velocity*2;
+			targetObject.enabled = true;
             selectedObject = null;
             print(velocity);
         }
@@ -46,10 +52,5 @@ public class DragAndDrop : MonoBehaviour
         {
             oldPos = selectedObject.transform.position;
         }
-    }
-
-    private void FixedUpdate()
-    {
-        
     }
 }
